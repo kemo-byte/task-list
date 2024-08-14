f1aab2c819d69f7a9dee2d4039720113fb00663e
kamal kafi <kamalkafi12@gmail.com>
2024-08-14 07:47:11 +0200

    lec 16

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return "Hello";
+})->name('hello');
+
+Route::get('/hello/{name}',function($name){
+    return "Hello $name";
+});
+
+Route::get('/hallo',function(){
+    return redirect()->route('hello');
+});
+
+Route::fallback(function(){
+    return 'stil got somewhere !';
+});
