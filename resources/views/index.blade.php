587494168e760d1f373f0f34602730e7bc0817bd
kamal kafi <kamalkafi12@gmail.com>
2024-08-14 08:38:26 +0200

    lec 17

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+<h1>Hello I'm a blade template</h1>
+@isset($name)
+    <h2> Name : {{ $name }} </h2>
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index',[
+        'name' => 'kamal'
+    ]);
 });
 
 Route::get('/hello', function(){
