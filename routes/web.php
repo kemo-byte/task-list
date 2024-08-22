03005dc15bbffaf119e04a1f5a3a180e10278e3b
kamal kafi <kamalkafi12@gmail.com>
2024-08-22 16:42:32 +0200

    lect 24

--- routes/web.php
+++ routes/web.php
@@ -57,20 +57,15 @@ Route::get('/', function() {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks){
+Route::get('/tasks', function (){
     return view('index',[
-        'tasks' => $tasks
+        'tasks' => \App\Models\Task::latest()->get()
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function($id) use ($tasks){
-    $task = collect($tasks)->firstWhere('id',$id);
+Route::get('/tasks/{id}', function($id){
 
-    if(!$task){
-        abort(Response::HTTP_NOT_FOUND);
-    }
-
-    return view('show', ['task'=> $task]);
+    return view('show', ['task'=> \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
 
 // Route::get('/hello', function(){
