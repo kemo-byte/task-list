92d9319f1cd10d7d54de27518e89f5d59589f16c
kamal kafi <kamalkafi12@gmail.com>
2024-08-23 21:23:01 +0200

    lec 28

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,4 +11,13 @@
 @if ($task->long_description)
     <p>{{  $task->long_description }}</p>
 @endif
+<p>
+    {{ $task->created_at }}
+</p>
+
+<p>
+    {{ $task->updated_at }}
+</p>
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,42 @@ Route::get('/tasks', function (){
 })->name('tasks.index');
 
 Route::post('/tasks', function(Request $request) {
-$data = $request->validate([
-    'title' => 'required|max:255',
-    'description' => 'required',
-    'long_description' => 'required'
-]);
-$task = new TasK;
-$task->title = $data['title'];
-$task->description = $data['description'];
-$task->long_description = $data['long_description'];
-$task->save();
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+    $task = new TasK;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
 
-return redirect()->route('tasks.show',['id' => $task->id])->with('success','Task created successfully!');
+    return redirect()->route('tasks.show',['id' => $task->id])->with('success','Task created successfully!');
 })->name('tasks.store');
 
+Route::put('/tasks/{id}', function($id,Request $request) {
+
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show',['id' => $task->id])->with('success','Task updated successfully!');
+})->name('tasks.update');
+
 Route::view('tasks/create','create')->name('tasks.create');
+
+Route::get('/tasks/{id}/edit', function($id){
+
+    return view('edit', ['task'=> \App\Models\Task::findOrFail($id)]);
+})->name('tasks.edit');
 
 Route::get('/tasks/{id}', function($id){
 
