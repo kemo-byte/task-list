1c657c0eab71f5a3457bd79133ca4baca2799645
kamal kafi <kamalkafi12@gmail.com>
2024-08-23 17:29:53 +0200

    lec 26

--- routes/web.php
+++ routes/web.php
@@ -4,55 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
 
-class Task{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at,
-    ){
-    }
-}
 
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ), new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ), new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ), new Task(
-        4,
-        'Take dos for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-];
 
 Route::get('/', function() {
     return redirect()->route('tasks.index');
@@ -65,8 +19,19 @@ Route::get('/tasks', function (){
 })->name('tasks.index');
 
 Route::post('/tasks', function(Request $request) {
-    dd($request->all());
-});
+$data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
+]);
+$task = new TasK;
+$task->title = $data['title'];
+$task->description = $data['description'];
+$task->long_description = $data['long_description'];
+$task->save();
+
+return redirect()->route('tasks.show',['id' => $task->id]);
+})->name('tasks.store');
 
 Route::view('tasks/create','create')->name('tasks.create');
 
